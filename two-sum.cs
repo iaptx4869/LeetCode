93240bf6ee5332aedb9645e1027fc5b757e396fd
Yixiang Jiang <j.1x@qq.com>
2016-09-07 16:48:32 +0800

    Update two-sum.cs

--- two-sum.cs
+++ two-sum.cs
@@ -6,14 +6,9 @@ public class Solution
         for (var i = 0; i < nums.Length; i++)
         {
             int x = nums[i];
-            if (ht.ContainsKey(target - x))
-            {
-                return new int[] { (int)ht[target - x], i };
-            }
-            if (!ht.ContainsKey(x))
-            {
-                ht.Add(x, i);
-            }
+            if (ht.ContainsKey(target - x)) return new int[] { (int)ht[target - x], i };
+            if (!ht.ContainsKey(x)) ht.Add(x, i);
+
         }
         return new int[] { 0, 0 };
     }
