2b15f02b314340bbb51865c522d2d06acefb740c
iaptx4869 <j.1x@qq.com>
2017-07-13 15:43:40 +0800

    update h-d.java

---
+++ hamming-distance.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int hammingDistance(int x, int y) {
+        int Distance = 0;
+        while (x != 0 || y != 0) {
+            if (x % 2 != y % 2) {
+                Distance++;
+            }
+            x /= 2;
+            y /= 2;
+        }
+        return Distance;
+    }
+}
