88abc5ccfc8996157d76058d822edc9cbcd95cd7
iaptx4869 <j.1x@qq.com>
2017-07-13 17:04:33 +0800

    Update h-d , Create m-t-b-t

--- hamming-distance.java
+++ hamming-distance.java
@@ -1,5 +1,11 @@
 public class Solution {
     public int hammingDistance(int x, int y) {
+        return Integer.bitCount(x ^ y);
+    }
+}
+
+public class Solution {
+    public int hammingDistance(int x, int y) {
         int Distance = 0;
         while (x != 0 || y != 0) {
             if (x % 2 != y % 2) {
