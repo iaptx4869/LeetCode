df02894ea26fb5652426b01b52386c55f79407e3
iaptx4869 <j.1x@qq.com>
2017-07-14 15:06:14 +0800

    Create a-p

---
+++ array-partition-i.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function (nums) {
+    var number = 0;
+    nums.sort(function (a, b) { return a - b });
+    for (var i = 0; i < nums.length; i = i + 2) {
+        number += nums[i];
+    }
+    return number;
+};
