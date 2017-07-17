33505f344e95985b8e8943da0ed1bd48e190dc49
iaptx4869 <j.1x@qq.com>
2017-07-17 10:43:17 +0800

    Update k-r

--- 500. Keyboard Row/Solution.java
+++ 500. Keyboard Row/Solution.java
@@ -1,5 +1,6 @@
 public class Solution {
     public String[] findWords(String[] words) {
-        return Stream.of(words).filter(s -> s.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*")).toArray(String[]::new);
+        return Stream.of(words).filter(s -> s.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*"))
+                .toArray(String[]::new);
     }
 }
