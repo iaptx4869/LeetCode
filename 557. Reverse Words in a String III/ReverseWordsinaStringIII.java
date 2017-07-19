ab9ee79109a32caac7b5b707598c245c424b4920
iaptx4869 <j.1x@qq.com>
2017-07-19 10:15:12 +0800

    Create r-w-i-a-s-i

---
+++ 557. Reverse Words in a String III/ReverseWordsinaStringIII.java
@@ -0,0 +1,7 @@
+public class ReverseWordsinaStringIII{
+    public static void main(String[] args) {
+        String Input="Let's take LeetCode contest";
+        String Output=new Solution().reverseWords(Input);
+        System.out.println(Output);
+    }
+}
